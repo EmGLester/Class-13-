7b15c56d955d0cf914e34c765af5c08c325a9bef
Emily Grace Lester <emhagerman5678@gmail.com>
2022-03-28 14:40:54 -0500

    more practice

---
+++ basic-event-binding-exercise/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-event-binding-exercise';
+  msg:string;
+  constructor() {
+
+   }
+
+   clickEvent(){
+    this.msg='Button is Clicked';
+    return this.msg;
+  }
+}
